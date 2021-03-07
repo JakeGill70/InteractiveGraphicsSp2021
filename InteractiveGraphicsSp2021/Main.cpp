ca893b7caf645ce8c26c15a6a9f91807a8a2d699
Jake Gillenwater <JakeGill70@gmail.com>
2021-03-07 02:53:19 -0500

    Milestone 1

--- InteractiveGraphicsSp2021/Main.cpp
+++ InteractiveGraphicsSp2021/Main.cpp
@@ -34,7 +34,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-   GLFWwindow* window = glfwCreateWindow(800, 600, "ETSU Computing Interactive Graphics", NULL, NULL);
+   GLFWwindow* window = glfwCreateWindow(800, 600, "Lab Week 7 - Interactive Graphics", NULL, NULL);
    if (window == NULL) {
       std::cout << "Failed to create GLFW window" << std::endl;
       glfwTerminate();
